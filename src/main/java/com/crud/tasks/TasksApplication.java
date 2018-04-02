91e2d20affde00f98e36a4bd5590a4cdd1dcda42
SlawomirR <no.for.spam@wp.pl>
2018-04-02 11:23:08 +0200

    changed hibernate dialect and disable port number

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -2,8 +2,6 @@ package com.crud.tasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class TasksApplication /*extends SpringBootServletInitializer*/ {
