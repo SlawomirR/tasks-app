e93e98623ddbdd8be696f8f81b2a4bed7d3101a8
SlawomirR <no.for.spam@wp.pl>
2018-04-11 23:11:59 +0200

    19.1 - experimented with @CrossOrigin annotation.

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/trello")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowCredentials = "*")
 public class TrelloController {
 
     @Autowired
