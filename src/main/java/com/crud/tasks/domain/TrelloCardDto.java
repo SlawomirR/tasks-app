a0cd1bb22c7571ab3fc4319c02003b54e966c94b
SlawomirR <no.for.spam@wp.pl>
2018-04-12 08:14:00 +0200

    19.1 - added @JsonProperty in TrelloCardDto class.

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,5 +1,7 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,10 +9,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloCardDto {
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("desc")
     private String description;
+
+    @JsonProperty("pos")
     private String pos;
+
+    @JsonProperty("idList")
     private String listId;
+
+    @JsonProperty("badges")
     private TrelloCardBadgesDto badges;
 }
