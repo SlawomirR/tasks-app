015e0e037ef41ea84ec00318d2aa43262775feac
SlawomirR <no.for.spam@wp.pl>
2018-04-14 21:19:13 +0200

    19.4 - added scheduling option for sending emails.

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -22,4 +22,7 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
 
     @Override
     void deleteById(Long id);
+
+    @Override
+    long count();
 }
---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+    public static final String SUBJECT = "Tasks: Once a day information email.";
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+//    @Scheduled(fixedDelay = 20_000)
+    @Scheduled(cron = "0 0 10 * * *")
+    public void sendInformationEmail() {
+        long size = taskRepository.count();
+        simpleEmailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                "",
+                SUBJECT,
+                "Currently in database you got: " + size + " tasks."
+        ));
+    }
+}
