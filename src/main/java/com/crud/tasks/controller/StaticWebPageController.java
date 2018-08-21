a2d757b2f6408517a6627b650d8447bb63834671
SlawomirR <no.for.spam@wp.pl>
2018-08-21 18:27:03 +0200

    Updated link to GitHub Pages with front-end for this application.

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -12,8 +12,8 @@ public class StaticWebPageController {
     public String index(Map<String, Object> model) {
         model.put("swagger_info", "https://mysterious-dusk-98078.herokuapp.com/swagger-ui.html");
         model.put("swagger_link", "https://mysterious-dusk-98078.herokuapp.com/swagger-ui.html");
-        model.put("url_info", "https://slawomirr.github.io");
-        model.put("url_link", "https://slawomirr.github.io");
+        model.put("url_info", "https://slawomirr.github.io/tasks-app_front-end");
+        model.put("url_link", "https://slawomirr.github.io/tasks-app_front-end");
         model.put("trello_info", "https://trello.com/b/r3PFBoTh");
         model.put("trello_link", "https://trello.com/b/r3PFBoTh");
         model.put("variable", "My sample Thymeleaf math calculations:");
